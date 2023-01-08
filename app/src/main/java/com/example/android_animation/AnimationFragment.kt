1011ef00e9536da6b02678d35b21923fb5dcb59f
SadullayevBotir <91020070+SadullayevBotir@users.noreply.github.com>
2023-01-08 15:21:24 +0500

    Initial commit

---
+++ app/src/main/java/com/example/android_animation/AnimationFragment.kt
@@ -0,0 +1,18 @@
+package com.example.android_animation
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class AnimationFragment :Fragment(R.layout.fragment_animation){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+
+
+    }
+}
